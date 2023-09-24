80ba07e8c12291417dd3aa1312f5651ec4ef255b
herpestes <ahmetbeyazoglu@gmail.com>
2023-09-24 21:20:00 +0300

    Day 5 & Design

--- Design/app/src/main/java/com/herpestes/design/MainActivity.kt
+++ Design/app/src/main/java/com/herpestes/design/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //yazılım kısmında R harfi res klasörünü temsil eder.
+        //xml kısmında @ işareti res klasöürünü temsil eder
+
+
     }
 }
