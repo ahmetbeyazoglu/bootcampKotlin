75b63ef90e7e1af66bfe1ac63442ecc116e7406a
herpestes <ahmetbeyazoglu@gmail.com>
2023-09-25 23:57:01 +0300

    Day5 & ColorPicker

---
+++ app/src/main/java/com/herpestes/bootcamp_kotlin/Design/app/src/main/java/com/herpestes/design/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.herpestes.design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.herpestes.bootcamp_kotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //yazılım kısmında R harfi res klasörünü temsil eder.
+        //xml kısmında @ işareti res klasöürünü temsil eder
+
+    }
+}
