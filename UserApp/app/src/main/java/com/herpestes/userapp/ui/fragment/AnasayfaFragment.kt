34f4b6a014413d4496c50608bf6c93eb5e08af34
herpestes <ahmetbeyazoglu@gmail.com>
2023-10-03 23:49:55 +0300

    Day 9 - UsersApp

--- UserApp/app/src/main/java/com/herpestes/userapp/ui/fragment/AnasayfaFragment.kt
+++ UserApp/app/src/main/java/com/herpestes/userapp/ui/fragment/AnasayfaFragment.kt
@@ -8,9 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView.OnQueryTextListener
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.herpestes.userapp.R
 import com.herpestes.userapp.data.entity.Kisiler
 import com.herpestes.userapp.databinding.FragmentAnasayfaBinding
+import com.herpestes.userapp.ui.adapter.KisilerAdapter
 
 
 class AnasayfaFragment : Fragment() {
@@ -19,6 +22,23 @@ class AnasayfaFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
+
+        binding.toolbarAnasayfa.title = "Anasayfa"
+
+        //binding.rv.layoutManager = LinearLayoutManager(requireContext())
+        binding.rv.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+        val kisilerListesi = ArrayList<Kisiler>()
+        val k1 = Kisiler(1,"Ahmet", "1111")
+        val k2 = Kisiler(2,"Zeynep", "2222")
+        val k3 = Kisiler(3,"Ali", "3333")
+        kisilerListesi.add(k1)
+        kisilerListesi.add(k2)
+        kisilerListesi.add(k3)
+
+        val kisilerAdapter = KisilerAdapter(requireContext(), kisilerListesi)
+        binding.rv.adapter = kisilerAdapter
+
 
         binding.fab.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.kisiKayitGecis)
